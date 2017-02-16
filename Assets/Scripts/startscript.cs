0046929b804950ef1b00b2311369bf23df0998f1
Unknown <tgr235@nyu.edu>
2017-02-16 14:39:29 -0500

    Initial Commit, ignored builds folder and .zip file

---
+++ Assets/Scripts/startscript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startscript : MonoBehaviour {
+
+
+	public GameObject sphere;
+
+	private bool check;
+
+	// Use this for initialization
+	void Start () {
+		check = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space) && check == false) {
+			Instantiate (sphere, transform.position, Quaternion.identity);
+			check = true;
+		}
+	}
+}
---
+++ Assets/Scripts/uiscript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class uiscript : MonoBehaviour {
+
+	string myText;
+	public GameObject clip;
+
+	// Use this for initialization
+	void Start () {
+		myText = "Press Space to Begin!";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space))
+		{
+			myText = "";
+		}
+	}
+
+	void OnGUI(){
+		GUI.Label (new Rect (150f, 150f, 600f, 300f), myText);
+	}
+		
+	void OnTriggerEnter (Collider other){
+		myText = "THE END";
+		Instantiate (clip, transform.position, Quaternion.identity);
+	}
+}
